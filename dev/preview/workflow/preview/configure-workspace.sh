ea1f096ad942da873365143cbf311339053ab0bc
Gero Posmyk-Leinemann <gero@gitpod.io>
2024-09-23 08:04:05 +0000

    automations for basic preview tasks
    WIP: IDP not working, yet

--- dev/preview/workflow/preview/configure-workspace.sh
+++ dev/preview/workflow/preview/configure-workspace.sh
@@ -19,13 +19,23 @@ if [ -z "${PREVIEW_ENV_DEV_SA_KEY_PATH:-}" ]; then
 fi
 
 if [ -f "/usr/local/gitpod/config/initial-spec.json" ]; then
+  # gcloud iam workload-identity-pools create-cred-config \
+  #   projects/184212049955/locations/global/workloadIdentityPools/app-gitpod/providers/app-gitpod-provider \
+  #   --service-account=preview-environmnet-dev@gitpod-dev-preview.iam.gserviceaccount.com \
+  #   --service-account-token-lifetime-seconds=1h \
+  #   --output-file="${PREVIEW_ENV_DEV_SA_KEY_PATH}" \
+  #   --executable-command='gitpod-new idp token --audience="//iam.googleapis.com/projects/184212049955/locations/global/workloadIdentityPools/app-gitpod/providers/app-gitpod-provider"  --gcloud' \
+  #   --executable-timeout-millis=5000
+
   gcloud iam workload-identity-pools create-cred-config \
-    projects/184212049955/locations/global/workloadIdentityPools/gitpod-next/providers/gitpod-next-provider \
+    projects/184212049955/locations/global/workloadIdentityPools/app-gitpod/providers/app-gitpod-provider \
     --service-account=preview-environmnet-dev@gitpod-dev-preview.iam.gserviceaccount.com \
-    --service-account-token-lifetime-seconds=1h \
-    --output-file="${PREVIEW_ENV_DEV_SA_KEY_PATH}" \
-    --executable-command='node /workspace/gitpod/dev/next-oidc/oidc.js' \
-    --executable-timeout-millis=5000
+    --subject-token-type="urn:ietf:params:oauth:token-type:jwt" \
+    --credential-source-type="json" \
+    --executable-command='gitpod-new idp token --audience="//iam.googleapis.com/projects/184212049955/locations/global/workloadIdentityPools/app-gitpod/providers/app-gitpod-provider"  --gcloud' \
+    --executable-timeout-millis=5000 \
+    --executable-output-file="/tmp/gcloud-oidc.json" \
+    --output-file="${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 elif [[ -n "${PREVIEW_ENV_DEV_CRED:-}" ]]; then
   echo "${PREVIEW_ENV_DEV_CRED}" >"${PREVIEW_ENV_DEV_SA_KEY_PATH}"
 fi
